ddfeeb83432af4a5bd1b0234aa8c34abe9c533f1
oko-do <kons.dolgov@gmail.com>
2026-04-09 11:16:30 +0200

    fix client logos showing alt text on first load

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -124,7 +124,8 @@ function MarqueeRow({ items, direction = "left", dragVelocityRef }: MarqueeRowPr
               src={client.logo}
               alt={client.name}
               draggable={false}
-              className="h-6 md:h-10 w-auto object-contain grayscale opacity-60 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300"
+              loading="eager"
+              className="h-6 md:h-10 w-auto min-w-[60px] md:min-w-[100px] object-contain grayscale opacity-60 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300 text-transparent"
             />
           </a>
         ))}
