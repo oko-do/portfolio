e0e6bea63fdf147e5b7642c129ae84ef4cf0ed26
oko-do <kons.dolgov@gmail.com>
2026-04-13 20:06:42 +0200

    fix: replace PreloadedImg with plain img in Tools to prevent icon loading race condition

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -3,7 +3,6 @@
 import { useTranslations } from "next-intl";
 import { tools } from "@/lib/portfolio-data";
 import { FadeIn } from "./FadeIn";
-import { PreloadedImg } from "./PreloadedImg";
 
 export default function Tools() {
   const t = useTranslations("Tools");
@@ -28,11 +27,13 @@ export default function Tools() {
                 className="flex flex-col items-center gap-3 p-4 rounded-lg hover:bg-secondary/50 transition-colors group"
               >
                 <div className="w-10 h-10 rounded-lg flex items-center justify-center">
-                  <PreloadedImg
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
                     src={tool.icon}
                     alt={tool.name}
                     width={32}
                     height={32}
+                    loading="eager"
                     className="w-8 h-8 object-contain brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300"
                   />
                 </div>
