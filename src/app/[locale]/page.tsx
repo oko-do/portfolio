5b9ff6d2e4ddeb08f36f9d2a48ca270945b9d8e0
oko-do <kons.dolgov@gmail.com>
2026-05-14 21:38:37 +0200

    chore: change home password to okodo1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,4 @@
 import { setRequestLocale } from "next-intl/server";
-import { getTranslations } from "next-intl/server";
-import { use } from "react";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
@@ -8,7 +6,6 @@ import Tools from "@/components/Tools";
 import Clients from "@/components/Clients";
 import Cases from "@/components/Cases";
 import Contacts from "@/components/Contacts";
-import { HomePasswordOverlay } from "@/components/HomePasswordOverlay";
 
 type Props = {
   params: Promise<{ locale: string }>;
@@ -17,32 +14,20 @@ type Props = {
 export default async function HomePage({ params }: Props) {
   const { locale } = await params;
   setRequestLocale(locale);
-  const t = await getTranslations("CasePage");
 
   return (
-    <HomePasswordOverlay
-      translations={{
-        title: t("homePasswordTitle"),
-        description: t("homePasswordDescription"),
-        placeholder: t("homePasswordPlaceholder"),
-        submit: t("homePasswordSubmit"),
-        error: t("homePasswordError"),
-        noPassword: t("homePasswordNoPassword"),
-      }}
-    >
-      <div className="mx-auto max-w-7xl">
-        <Hero />
-        <About />
-        <Experience />
-        <Tools />
-        {/* Clients marquee breaks out of container for full-bleed */}
-        <div className="relative -mx-[calc((100vw-100%)/2)] w-screen">
-          <Clients />
-        </div>
-        <Cases />
-        <Contacts />
+    <div className="mx-auto max-w-7xl">
+      <Hero />
+      <About />
+      <Experience />
+      <Tools />
+      {/* Clients marquee breaks out of container for full-bleed */}
+      <div className="relative -mx-[calc((100vw-100%)/2)] w-screen">
+        <Clients />
       </div>
-    </HomePasswordOverlay>
+      <Cases />
+      <Contacts />
+    </div>
   );
 }
 
